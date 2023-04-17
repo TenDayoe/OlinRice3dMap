2853cad848503eac461c1764e918b69f47ce105f
TenDayoe <tdayoe@macalester.edu>
2023-04-16 23:50:59 -0500

    User testing ready
    Features added :
    
    Character goes in straight path and takes 90 degree turns most of the time.
    
    Now navigation can also be done with elevators.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -13,12 +13,51 @@ public class playerController : MonoBehaviour
     public Camera camera;
     public TMP_Text floorText;
     public Vector3 tempForwardHolder;
+    public Vector3[] currentCorner; 
+    public GameObject elevator;
+    //ElevatorFeatures
+
+    public string elev1; 
+    public string elev2;
+    public bool floorReached ;
+    public bool elev1Reached = false;
+    public char currentFloor; 
+    public char destFloor;
+    public string usingElevator;
+    private string currentLocation ; 
+    private string destinationLocation ;
+
+    
+    public Camera elevatorCam;
     private void Start()
     {
-    
-        string currentLocation = "OL" + PlayerPrefs.GetString("currentLocation").Substring(0, PlayerPrefs.GetString("currentLocation").Length - 1);
-        string destinationLocation = "OL"+ PlayerPrefs.GetString("destination").Substring(0, PlayerPrefs.GetString("destination").Length -1 );
         
+        currentLocation = "OL" + PlayerPrefs.GetString("currentLocation").Substring(0, PlayerPrefs.GetString("currentLocation").Length - 1);
+        destinationLocation = "OL"+ PlayerPrefs.GetString("destination").Substring(0, PlayerPrefs.GetString("destination").Length -1 );
+        
+        //Elevator Features 
+        // string usingElevator = PlayerPrefs.GetString("usingElevator").Substring(0, PlayerPrefs.GetString("currentLocation").Length - 1);
+        usingElevator = "yes";
+        floorReached = false;
+        if (usingElevator == "yes"){
+            
+            Debug.Log("Using Elevator");
+            currentFloor = currentLocation[2];
+            destFloor = destinationLocation[2];
+
+            if (currentFloor !=destFloor ){
+                elev1 = "ElevatorFloorMarker" + currentFloor;
+                elev2 = "ElevatorFloorMarker" + destFloor;
+                destinationLocation = elev1;
+                
+            }else{
+                usingElevator = "no";
+            }
+
+        }
+        //
+        
+
         transform.position = GameObject.Find(currentLocation).transform.position;
         agent.Warp(transform.position);
         dummy.Warp(transform.position);
@@ -32,7 +71,7 @@ public class playerController : MonoBehaviour
         Vector3 destinationPointerPos = destination.transform.position;
         destinationPointerPos.y -= 0.9f;
         destinationPointer.transform.position = destinationPointerPos;
-
+        
     }
     
      
@@ -65,22 +104,63 @@ public class playerController : MonoBehaviour
         if (agent.path != null)
         {
             lineRenderer.startWidth = 0.3f;
-        lineRenderer.endWidth = 0.3f;
+            lineRenderer.endWidth = 0.3f;
             lineRenderer.positionCount = agent.path.corners.Length;
             lineRenderer.SetPositions(agent.path.corners);
-            
+
             if (Vector3.Dot(tempForwardHolder, dummy.transform.forward) <0.2f){
                 Vector3 newReference = dummy.transform.position; 
                 Vector3 agentforwardVector= agent.transform.forward; 
                 Vector3 dummyforwardVector = agent.transform.forward; 
-                
-
-
-
                 tempForwardHolder = dummy.transform.forward;
                 Debug.Log("Oh dummy has turned ");
-                
             }
+
+            
+        }
+        
+        elevatorManagement();
+    }
+
+    void elevatorManagement(){
+        if(usingElevator == "yes" && floorReached == false){
+            
+            
+            if(Vector3.Distance(transform.position, GameObject.Find(elev1).transform.position)<2f && elev1Reached == false){
+                elevator.GetComponent<elevator>().floorLevel = int.Parse(destFloor.ToString());
+                elev1Reached = true;
+                Debug.Log("Reached Elev1");
+                camera.gameObject.SetActive(false);
+                elevatorCam.gameObject.SetActive(true);
+                
+                agent.isStopped = true;
+                Vector3 tempMarkerPos = new Vector3(-10,-10,-10);
+                GameObject.Find("DestinationPointer").transform.position = tempMarkerPos;
+            }
+            if(elevator.GetComponent<elevator>().currentFloor == elevator.GetComponent<elevator>().floorLevel && elevator.GetComponent<elevator>().floorLevel == int.Parse(destFloor.ToString())){
+                Debug.Log("Destination reached");
+                floorReached = true; 
+                elevatorCam.gameObject.SetActive(false);
+                camera.gameObject.SetActive(true);
+                
+                
+                //setting the destination location
+                destinationLocation = "OL"+ PlayerPrefs.GetString("destination").Substring(0, PlayerPrefs.GetString("destination").Length -1 );
+                destination = GameObject.Find(destinationLocation);
+
+                //setting the starting location
+                
+                transform.position = GameObject.Find(elev2).transform.position;
+                agent.Warp(transform.position);
+                dummy.Warp(transform.position);
+                agent.SetDestination(destination.transform.position);
+                dummy.SetDestination(destination.transform.position);
+                tempForwardHolder = agent.transform.forward;
+                Vector3 destinationPointerPos = destination.transform.position;
+                destinationPointerPos.y -= 0.9f;
+                GameObject.Find("DestinationPointer").transform.position = destinationPointerPos;
+            }
+           
         }
     }
 }
