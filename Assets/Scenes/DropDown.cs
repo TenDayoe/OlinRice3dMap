49e7edd5dbbda84c43abd6a65da456046110f081
TenDayoe <tdayoe@macalester.edu>
2023-04-26 21:59:33 -0500

    urp

---
+++ Assets/Scenes/DropDown.cs
@@ -0,0 +1,31 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+
+// public class DropDown : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public GameObject output;
+//     public void HandleInputData(int val)
+//     {
+//         if (val==0) {
+//             output.text = "I love minecraft";
+//         }
+
+//         if (val==1) {
+//             output.text = "I love fortni";
+//         }
+
+//         if (val==2) {
+//             output.text = "I love csgo";
+//         }
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
