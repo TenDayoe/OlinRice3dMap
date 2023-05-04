f5f2c1cced476e138698297154ac6c00d69e4f49
BraydenCoronado <bcoronad@macalester.edu>
2023-05-03 23:23:42 -0500

    Clearing the main to merge

--- Assets/Scripts/RoomInfo.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class RoomInfo
-{
-    public string Number;
-}
--- Assets/Scripts/RoomNumbers.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class RoomNumbers
-{
-    public RoomInfo[] roomNumbers;
-}
